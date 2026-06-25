ef059d589b1f03fbcb59a5148b15c7b0006d857a
jdblair78 <jdblair78@gmail.com>
2026-06-25 15:13:02 -0400

    Navbar logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
 import "./App.css";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Login />} />
         </Routes>
