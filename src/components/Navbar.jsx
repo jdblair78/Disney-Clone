2049737533adba2beaeafce0f37458ca7188ed24
jdblair78 <jdblair78@gmail.com>
2026-06-25 15:51:05 -0400

    navbar list items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,46 +1,129 @@
 import styled from "styled-components";
 
 const Navbar = () => {
-    return(
-<Nav>
-    <Logo>
+  return (
+    <Nav>
+      <Logo>
         <img src="/images/logo.svg" alt="Disney" />
-    </Logo>
-</Nav>
-    )
-}
+      </Logo>
+      <NavMenu>
+        <a href="/home">
+          <img src="/images/home-icon.svg" alt="Home" />
+          <span>HOME</span>
+        </a>
+        <a href="/home">
+          <img src="/images/search-icon.svg" alt="Home" />
+          <span>SEARCH</span>
+        </a>
+        <a href="/home">
+          <img src="/images/watchlist-icon.svg" alt="Home" />
+          <span>WATCHLIST</span>
+        </a>
+        <a href="/home">
+          <img src="/images/original-icon.svg" alt="Home" />
+          <span>ORIGINALS</span>
+        </a>
+        <a href="/home">
+          <img src="/images/movie-icon.svg" alt="Home" />
+          <span>MOVIES</span>
+        </a>
+        <a href="/home">
+          <img src="/images/series-icon.svg" alt="Home" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+    </Nav>
+  );
+};
 
 const Nav = styled.nav`
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    height: 70px;
-    background-color: #090b13;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 36px;
-    letter-spacing: 16px;
-    z-index: 3;
-`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 3;
+`;
 const Logo = styled.a`
-    padding: 0;
-    width: 80px;
-    margin-top: 4px;
-    max-height: 70px;
-    font-size: 0;
-    display: inline-block;
+  padding: 0;
+  width: 80px;
+  margin-top: 4px;
+  max-height: 70px;
+  font-size: 0;
+  display: inline-block;
+  img {
+    display: block;
+    width: 100%;
+  }
+`;
+
+const NavMenu = styled.div`
+  align-items: center;
+  display: flex;
+  flex-flow: row nowrap;
+  height: 100%;
+  justify-content: flex-end;
+  margin: 0;
+  padding: 0;
+  position: relative;
+  margin-right: auto;
+  margin-left: 25px;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+
     img {
-        display: block;
-        width: 100%;
+      height: 20px;
+      min-width: 20px;
+      width: 20px;
+      z-index: auto;
     }
 
-`
+    span {
+      color: rgb(249, 249, 249);
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      line-height: 1.08;
+      padding: 2px 0px;
+      white-space: nowrap;
+      position: relative;
 
+      &:before {
+        background-color: rgb(249, 249, 249);
+        border-radius: 0px 0px 4px 4px;
+        bottom: -6px;
+        content: "";
+        opacity: 0;
+        height: 2px;
+        left: 0;
+        position: absolute;
+        right: 0px;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.46, 0.94);
+        width: auto;
+      }
+    }
 
-
-
-
+    &:hover {
+      span:before {
+        transform: scaleX(1);
+        visibility: visible;
+        opacity: 1;
+      }
+    }
+  }
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
 
 export default Navbar;
