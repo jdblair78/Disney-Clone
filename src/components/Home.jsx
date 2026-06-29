34c1abd162236bfe4e820bac17b50a880e526ec3
jdblair78 <jdblair78@gmail.com>
2026-06-29 16:47:34 -0400

    installed slider

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,35 @@
 import styled from "styled-components";
+import ImgSlider from "./ImgSlider";
 
 const Home = () => {
-    return (
-        <div>
+  return (
+    <Container>
+      <ImgSlider />
+    </Container>
+  );
+};
 
-        </div>
-    )
-}
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+  
 
+  &::after {
+    content: "";
+    position: absolute;
+    inset: 0px;
+    background-image: url("/images/home-background.png");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
 
-
-export default Home; 
+export default Home;
